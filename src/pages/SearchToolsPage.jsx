1a8acaf530c1fa219b144abf3db277614310850b
anarehder <anapaula.rehder@gmail.com>
2023-11-19 09:45:10 -0300

    feat: Static tools on search tools page

--- src/pages/SearchToolsPage.jsx
+++ src/pages/SearchToolsPage.jsx
@@ -8,7 +8,42 @@ function SearchTool(){
                 Barra de Busca
             </SearchBarForm>
             <Tools>
-                Lista de Ferramentas
+                <ToolBlock>
+                    Ferramenta 1
+                </ToolBlock>
+                <ToolBlock>
+                    Ferramenta 2
+                </ToolBlock>
+                <ToolBlock>
+                    Ferramenta 3
+                </ToolBlock>
+                <ToolBlock>
+                    Ferramenta 4
+                </ToolBlock>
+                <ToolBlock>
+                    Ferramenta 5
+                </ToolBlock>
+                <ToolBlock>
+                    Ferramenta 6
+                </ToolBlock>
+                <ToolBlock>
+                    Ferramenta 7
+                </ToolBlock>
+                <ToolBlock>
+                    Ferramenta 8
+                </ToolBlock>
+                <ToolBlock>
+                    Ferramenta 9
+                </ToolBlock>
+                <ToolBlock>
+                    Ferramenta 10
+                </ToolBlock>
+                <ToolBlock>
+                    Ferramenta 11
+                </ToolBlock>
+                <ToolBlock>
+                    Ferramenta 12
+                </ToolBlock>
             </Tools>
         </Container>
     )
@@ -18,7 +53,7 @@ export default SearchTool;
 
 const Container = styled.div`
   width: 100%;
-  padding: 15px;
+  padding: 15px 350px;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -30,10 +65,27 @@ const Container = styled.div`
 const SearchBarForm = styled.form`
     height: 50px;
     width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: flex-start;
     background-color: blue;
 `
 const Tools = styled.div`
-    height: 300px;
-    width: 100%;
+    padding: 15px 50px;
     background-color: red;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    gap: 30px;
+`
+
+const ToolBlock = styled.div`
+    width: 200px;
+    height: 200px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: gray;
 `
